d3407b99ccf7e44ea0bad313f8a40fea832d67bf
michaell <michaell@i9technologies.com>
2018-09-27 10:58:50 -0600

    Stateful components and introduction of property initializers in lieu of explicit constructor binding.

--- fullstack-react-code/voting_app/public/js/app.js
+++ fullstack-react-code/voting_app/public/js/app.js
@@ -1,10 +1,36 @@
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state =  {
+      products: []
+    };
+
+    // bind function to the context of the ProductList class/component
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
+  }
+
   handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.');
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts
+    })
   }
 
   render() {
-    const products = Seed.products.sort((a, b) => (
+    const products = this.state.products.sort((a, b) => (
       b.votes - a.votes
     ));
     const productComponents = products.map((product) => (
@@ -30,13 +56,7 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleUpVote = this.handleUpVote.bind(this);
-  }
-
-  handleUpVote() {
+  handleUpVote = () => {
     this.props.onVote(this.props.id);
   }
 
